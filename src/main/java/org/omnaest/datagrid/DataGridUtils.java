146445b9e0463fa12a87007e06b16bcb10625f96
Omnaest <omnaest@web.de>
2020-06-22 08:17:24 +0200

    enhanced DataGridUtils

--- src/main/java/org/omnaest/datagrid/DataGridUtils.java
+++ src/main/java/org/omnaest/datagrid/DataGridUtils.java
@@ -3,6 +3,8 @@ package org.omnaest.datagrid;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
+import javax.cache.Cache;
+
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteAtomicLong;
 import org.apache.ignite.IgniteCache;
@@ -13,55 +15,118 @@ import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.DataStorageConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.configuration.WALMode;
+import org.omnaest.utils.ComparatorUtils;
+import org.omnaest.utils.EnumUtils;
 import org.omnaest.utils.JSONHelper;
+import org.omnaest.utils.JSONHelper.JsonStringConverter;
 import org.omnaest.utils.StreamUtils;
+import org.omnaest.utils.optional.NullOptional;
 import org.omnaest.utils.repository.CoreElementRepository;
 import org.omnaest.utils.repository.ElementRepository;
 import org.omnaest.utils.repository.IndexElementRepository;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class DataGridUtils
 {
     public static interface DataGrid extends AutoCloseable
     {
-        public <D> IndexElementRepository<D> newIndexRepository(String name, Class<D> type);
+        public <D> IndexElementRepository<D> newIndexRepository(String name, Class<D> type, Class<?>... genericParameterTypes);
 
-        public <I, D> ElementRepository<I, D> newRepository(String name, Class<D> type, Function<Long, I> idSupplier);
+        public <I, D> ElementRepository<I, D> newRepository(String name, Function<Long, I> idSupplier, Class<D> type, Class<?>... genericParameterTypes);
+    }
+
+    private static class Data
+    {
+        @JsonProperty
+        private String value;
+
+        @JsonProperty
+        private long index;
+
+        public Data(String value, long index)
+        {
+            super();
+            this.value = value;
+            this.index = index;
+        }
+
+        protected Data()
+        {
+            super();
+        }
+
+        public String getValue()
+        {
+            return this.value;
+        }
+
+        public long getIndex()
+        {
+            return this.index;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "Data [value=" + this.value + ", index=" + this.index + "]";
+        }
+
     }
 
     public static DataGrid newLocalInstance()
     {
         DataStorageConfiguration dataStorageConfiguration = new DataStorageConfiguration();
         dataStorageConfiguration.setWalMode(WALMode.FSYNC);
+        dataStorageConfiguration.setWalSegmentSize(16 * 1024 * 1024);
+        dataStorageConfiguration.getDefaultDataRegionConfiguration()
+                                .setPersistenceEnabled(true);
         Ignite ignite = Ignition.start(new IgniteConfiguration().setClientMode(false)
+                                                                .setAutoActivationEnabled(true)
+                                                                .setConsistentId("testid")
                                                                 .setDataStorageConfiguration(dataStorageConfiguration)
                                                                 .setPeerClassLoadingEnabled(false));
+        ignite.cluster()
+              .active(true);
+
         return new DataGrid()
         {
             @Override
-            public <D> IndexElementRepository<D> newIndexRepository(String name, Class<D> type)
+            public <D> IndexElementRepository<D> newIndexRepository(String name, Class<D> type, Class<?>... genericParameterTypes)
             {
-                return IndexElementRepository.of(this.newRepository(name, type, (id) ->
-                {
-                    return id;
-                }));
+                return IndexElementRepository.of(this.newRepository(name, (id) -> id, type, genericParameterTypes));
             }
 
             @Override
-            public <I, D> ElementRepository<I, D> newRepository(String name, Class<D> type, Function<Long, I> idSupplier)
+            public <I, D> ElementRepository<I, D> newRepository(String name, Function<Long, I> idSupplier, Class<D> type, Class<?>... genericParameterTypes)
             {
-                IgniteAtomicLong atomicLong = ignite.atomicLong(name, 0l, true);
-                CacheConfiguration<I, String> cacheConfiguration = new CacheConfiguration<>();
+                IgniteAtomicLong indexIdCounter = ignite.atomicLong(name + ".index.id", 0l, true);
+                IgniteAtomicLong dataIdCounter = ignite.atomicLong(name + ".data.id", 0l, true);
+
+                CacheConfiguration<I, Data> cacheConfiguration = new CacheConfiguration<>();
                 cacheConfiguration.setName(name);
-                cacheConfiguration.setCacheMode(CacheMode.LOCAL);
-                cacheConfiguration.setWriteSynchronizationMode(CacheWriteSynchronizationMode.PRIMARY_SYNC);
-                IgniteCache<I, String> cache = ignite.getOrCreateCache(cacheConfiguration);
+                cacheConfiguration.setCacheMode(CacheMode.PARTITIONED);
+                cacheConfiguration.setWriteSynchronizationMode(CacheWriteSynchronizationMode.FULL_SYNC);
+
+                IgniteCache<I, Data> cache = ignite.getOrCreateCache(cacheConfiguration);
+                //                ignite.cluster()
+                //                      .enableWal(name);
+
+                JsonStringConverter<D> converter = JSONHelper.converter(type, genericParameterTypes)
+                                                             .withExceptionHandler(e ->
+                                                             {
+                                                                 throw new IllegalStateException(e);
+                                                             });
+
                 @SuppressWarnings("resource")
                 CoreElementRepository<I, D> coreElementRepository = new CoreElementRepository<I, D>()
                 {
                     @Override
                     public void put(I id, D element)
                     {
-                        cache.put(id, JSONHelper.prettyPrint(element));
+                        cache.put(id, new Data(converter.serializer()
+                                                        .apply(element),
+                                               dataIdCounter.getAndIncrement()));
                     }
 
                     @Override
@@ -71,9 +136,12 @@ public class DataGridUtils
                     }
 
                     @Override
-                    public D get(I id)
+                    public NullOptional<D> get(I id)
                     {
-                        return JSONHelper.readFromString(cache.get(id), type);
+                        return NullOptional.ofNullable(cache.getEntry(id))
+                                           .mapToNullable(entry -> entry.getValue())
+                                           .mapToNullable(data -> converter.deserializer()
+                                                                           .apply(data.getValue()));
                     }
 
                     @Override
@@ -83,10 +151,25 @@ public class DataGridUtils
                     }
 
                     @Override
-                    public Stream<I> ids()
+                    public Stream<I> ids(IdOrder idOrder)
                     {
-                        return StreamUtils.fromIterable(cache)
-                                          .map(entry -> entry.getKey());
+                        return EnumUtils.decideOn(idOrder)
+                                        .ifEqualTo(IdOrder.FROM_NEWEST_TO_OLDEST, () -> StreamUtils.fromIterable(cache)
+                                                                                                   .sorted(ComparatorUtils.builder()
+                                                                                                                          .of(Cache.Entry.class)
+                                                                                                                          .with(entry -> ((Data) entry.getValue()).getIndex())
+                                                                                                                          .reverse()
+                                                                                                                          .build())
+                                                                                                   .map(entry -> entry.getKey()))
+                                        .orIfEqualTo(IdOrder.FROM_OLDEST_TO_NEWEST, () -> StreamUtils.fromIterable(cache)
+                                                                                                     .sorted(ComparatorUtils.builder()
+                                                                                                                            .of(Cache.Entry.class)
+                                                                                                                            .with(entry -> ((Data) entry.getValue()).getIndex())
+                                                                                                                            .build())
+                                                                                                     .map(entry -> entry.getKey()))
+                                        .orIfEqualTo(IdOrder.ARBITRARY, () -> StreamUtils.fromIterable(cache)
+                                                                                         .map(entry -> entry.getKey()))
+                                        .orElseThrow(() -> new IllegalArgumentException("Unsupported IdOrder value: " + idOrder));
                     }
 
                     @Override
@@ -101,15 +184,18 @@ public class DataGridUtils
                     {
                         CoreElementRepository.super.close();
                         cache.close();
-                        atomicLong.close();
+                        indexIdCounter.close();
+                        dataIdCounter.close();
                     }
                 };
-                return coreElementRepository.toElementRepository(() -> idSupplier.apply(atomicLong.getAndIncrement()));
+                return coreElementRepository.toElementRepository(() -> idSupplier.apply(indexIdCounter.getAndIncrement()));
             }
 
             @Override
             public void close()
             {
+                ignite.cluster()
+                      .active(false);
                 ignite.close();
             }
         };
--- src/test/java/org/omnaest/datagrid/DataGridUtilsTest.java
+++ src/test/java/org/omnaest/datagrid/DataGridUtilsTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.junit.Test;
 import org.omnaest.datagrid.DataGridUtils.DataGrid;
@@ -19,31 +20,48 @@ public class DataGridUtilsTest
     {
         try (DataGrid dataGrid = DataGridUtils.newLocalInstance())
         {
-            try (IndexElementRepository<Map> repository = dataGrid.newIndexRepository("test", Map.class))
+            try (IndexElementRepository<Map> repository = dataGrid.newIndexRepository("test", Map.class, String.class, String.class))
             {
-
+                System.out.println("----");
                 repository.ids()
                           .forEach(id ->
                           {
                               System.out.println("ID: " + id);
                           });
+                repository.entries()
+                          .forEach(entry ->
+                          {
+                              System.out.println(entry);
+                          });
+
+                System.out.println("----");
+
+                repository.clear();
 
                 Long id1 = repository.add(MapUtils.builder()
                                                   .put("key1", "value1")
                                                   .put("key2", "value2")
                                                   .build());
+                System.out.println(id1);
                 Long id2 = repository.add(MapUtils.builder()
                                                   .put("key3", "value3")
                                                   .put("key4", "value4")
                                                   .build());
-                System.out.println(id1);
-                repository.put(3l, MapUtils.builder()
-                                           .put("key5", "value5")
-                                           .put("key6", "value6")
-                                           .build());
+                System.out.println(id2);
+
+                Long id3 = 3l;
+                repository.put(id3, MapUtils.builder()
+                                            .put("key5", "value5")
+                                            .put("key6", "value6")
+                                            .build());
+
+                System.out.println(repository.ids()
+                                             .collect(Collectors.toList()));
+
+                System.out.println("----");
 
                 assertEquals(3, repository.size());
-                assertEquals(Arrays.asList(id1, id2, 3l)
+                assertEquals(Arrays.asList(id1, id2, id3)
                                    .stream()
                                    .collect(Collectors.toSet()),
                              repository.ids()
@@ -52,17 +70,30 @@ public class DataGridUtilsTest
                                      .put("key1", "value1")
                                      .put("key2", "value2")
                                      .build(),
-                             repository.get(id1));
+                             repository.getValue(id1));
                 assertEquals(MapUtils.builder()
                                      .put("key3", "value3")
                                      .put("key4", "value4")
                                      .build(),
-                             repository.get(id2));
+                             repository.getValue(id2));
                 assertEquals(MapUtils.builder()
                                      .put("key5", "value5")
                                      .put("key6", "value6")
                                      .build(),
-                             repository.get(3l));
+                             repository.getValue(id3));
+
+                System.out.println("----");
+                repository.clear();
+                IntStream.range(0, 1000)
+                         .parallel()
+                         .forEach(ii ->
+                         {
+                             repository.add(MapUtils.builder()
+                                                    .put("key" + ii, "value" + ii)
+                                                    .build());
+                             System.out.println(ii);
+                         });
+                assertEquals(1000, repository.size());
             }
 
         }
